e01bc6685bdf61e96ebbae21a5c6bd8462286202
Kaushal <kaushal2469@gmail.com>
2024-02-19 12:25:18 +0530

    create library with smart and ui folder structure

---
+++ libs/ngrx/login-form/smart/README.md
@@ -0,0 +1,7 @@
+# login-form-smart
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test login-form-smart` to execute the unit tests.
---
+++ libs/ngrx/login-form/smart/src/lib/entry-component/login-form/login-form.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngx-login-form',
+  templateUrl: './login-form.component.html',
+  styleUrl: './login-form.component.scss',
+})
+export class LoginFormComponent {}
